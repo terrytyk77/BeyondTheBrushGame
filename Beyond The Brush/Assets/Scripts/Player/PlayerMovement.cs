3b896b5ccb3dfbba1b2d99dbeff455356da2fb51
DESKTOP-O32329F\Darkness <terrytyk77@gmail.com>
2020-11-21 22:59:39 +0000

    a

---
+++ Beyond The Brush/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using SpriterDotNetUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityAnimator animator = gameObject.GetComponent<SpriterDotNetBehaviour>().Animator;
+        IEnumerable<string> animations = animator.GetAnimations();
+
+        Debug.Log(animations);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float xMove = Input.GetAxis("Horizontal");
+
+        if(xMove != 0)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMove, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+    }
+}
