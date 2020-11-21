df26b37a717ebd03d9406feb5ab40729a4819ad2
prooheckcp <vasco.soares.2001@gmail.com>
2020-11-21 16:02:35 +0000

    Drawing Recognition
    Now it can detect what you drew

---
+++ Beyond The Brush/Assets/Scripts/Drawing/OnDrawEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GestureRecognizer;
+using UnityEngine;
+
+public class OnDrawEvent : MonoBehaviour
+{
+	public void OnRecognize(RecognitionResult result)
+	{
+		StopAllCoroutines();
+
+		
+		//Handle the shape
+		if (result != RecognitionResult.Empty)
+		{
+			Debug.Log(result.gesture.id);
+		}
+        else
+        {
+			Debug.Log("Doesn't know the shape");
+        }
+
+	}
+
+}
