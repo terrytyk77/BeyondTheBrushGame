af98dcfc64d234ae3b061643bdba16ac7c2d60d5
prooheckcp <vasco.soares.2001@gmail.com>
2020-11-21 22:52:39 +0000

    fuck this library

--- Beyond The Brush/Assets/Scripts/Player/PlayerMovement.cs
+++ Beyond The Brush/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,17 +1,14 @@
-﻿using SpriterDotNetUnity;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        UnityAnimator animator = gameObject.GetComponent<SpriterDotNetBehaviour>().Animator;
-        IEnumerable<string> animations = animator.GetAnimations();
 
-        Debug.Log(animations);
     }
 
     // Update is called once per frame
