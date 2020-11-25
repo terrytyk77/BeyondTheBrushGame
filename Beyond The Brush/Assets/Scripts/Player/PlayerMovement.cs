d4203a31560503460c8d7023c5d74514ca9264c8
Terry Keyrouz <61691608+terrytyk77@users.noreply.github.com>
2020-11-25 12:38:33 +0000

    Anim

--- Beyond The Brush/Assets/Scripts/Player/PlayerMovement.cs
+++ Beyond The Brush/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     //Variables||
 
         public float playerSpeed = 4f;
+        Rigidbody2D playerBody;
+        GameObject playerVertical;
+        GameObject playerHorizontal;
     //_________||
 
 
@@ -15,14 +18,15 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        playerVertical  = transform.Find("Vertical").gameObject;
+        playerHorizontal = transform.Find("Horizontal").gameObject;
     }
 
     // Fixed update is called 50 times per frame
     void FixedUpdate()
     {
         //Get the player rigid body component
-        Rigidbody2D playerBody = gameObject.GetComponent<Rigidbody2D>();
+        playerBody = gameObject.GetComponent<Rigidbody2D>();
 
         //Player movement magnitude
         float movementMagnitude = playerSpeed;
@@ -32,30 +36,52 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKey("w") && !Input.GetKey("s"))
         {
+            playerVerticalPerspective();
             newForce.y = movementMagnitude;
+
         }else if (Input.GetKey("s") && !Input.GetKey("w"))
         {
+            playerVerticalPerspective();
             newForce.y = -movementMagnitude;
         }else if (Input.GetKey("a") && !Input.GetKey("d"))
         {
+            playerHorizontalPerspective();
             newForce.x = -movementMagnitude;
+            playerHorizontal.transform.rotation = new Quaternion(0,180,0,1);
 
             //Change the image side
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+           
         }else if (Input.GetKey("d") && !Input.GetKey("a"))
         {
+            playerHorizontalPerspective();
             newForce.x = movementMagnitude;
+            playerHorizontal.transform.rotation = new Quaternion(0, 0, 0, 1);
 
             //Change the image side
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+
         }
-
-
-
-
+        else
+        {
+            playerHorizontal.GetComponent<Animator>().SetBool("Moving", false);
+            playerVertical.GetComponent<Animator>().SetBool("Moving", false);
+        }
 
         //Apply the force on the player's body
         playerBody.velocity = newForce;
 
+    }
+
+    private void playerVerticalPerspective()
+    {
+        playerVertical.SetActive(true);
+        playerHorizontal.SetActive(false);
+        playerVertical.GetComponent<Animator>().SetBool("Moving", true);
+    }
+
+    private void playerHorizontalPerspective()
+    {
+        playerHorizontal.SetActive(true);
+        playerVertical.SetActive(false);
+        playerHorizontal.GetComponent<Animator>().SetBool("Moving", true);
     }
 }
