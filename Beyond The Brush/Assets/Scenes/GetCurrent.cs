c427048d90bdf39fa899bad93ce60abda7ca42e1
prooheckcp <vasco.soares.2001@gmail.com>
2020-11-29 01:03:20 +0000

    Basic Game openinig
    now u can start the game in offline mode!

---
+++ Beyond The Brush/Assets/Scenes/GetCurrent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GetCurrent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int y = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log(y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Beyond The Brush/Assets/Scripts/MainMenu/ButtonEvents.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonEvents : MonoBehaviour
+{
+
+    private int MainGame = 0;
+
+    public void OpenNotification()
+    {
+            //This is gonna show up the confirmation box
+            gameObject.SetActive(!gameObject.active);
+    }
+
+    public void StartOffline()
+    {
+        //Load the scene
+        StartCoroutine(LoadAsync());
+    }
+
+    public void CancelOffline()
+    {
+        //This closes the window
+        gameObject.SetActive(false);
+    }
+
+    IEnumerator LoadAsync()
+    {
+        AsyncOperation opereration = SceneManager.LoadSceneAsync(MainGame);
+
+        while (!opereration.isDone)
+        {
+            Debug.Log(opereration.progress);
+
+            yield return null;
+        }
+
+    }
+
+}
+
