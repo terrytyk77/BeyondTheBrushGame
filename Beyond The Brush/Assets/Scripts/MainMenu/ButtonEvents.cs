321cb88ad3d27e4566af6395c4cbe5c86e795e2c
prooheckcp <vasco.soares.2001@gmail.com>
2020-11-29 01:41:27 +0000

    Working on the loading screen
    Doing some loading screen for between scenes

--- Beyond The Brush/Assets/Scripts/MainMenu/ButtonEvents.cs
+++ Beyond The Brush/Assets/Scripts/MainMenu/ButtonEvents.cs
@@ -16,8 +16,16 @@ public class ButtonEvents : MonoBehaviour
 
     public void StartOffline()
     {
-        //Load the scene
-        StartCoroutine(LoadAsync());
+        //Get the loading screen
+        GameObject loadingScreen = GameObject.FindGameObjectWithTag("LoadingScreen");
+
+        if (loadingScreen != null)
+            //Start Loading the Scene
+            StartCoroutine(LoadAsync(loadingScreen));
+        else
+            Debug.Log("Error! Scene not found");
+       
+        
     }
 
     public void CancelOffline()
@@ -26,14 +34,38 @@ public class ButtonEvents : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    IEnumerator LoadAsync()
+    IEnumerator LoadAsync(GameObject loadingScreen)
     {
+
+        //Getting the loading bar
+        GameObject background = loadingScreen.GetComponent<Transform>().GetChild(0).gameObject;
+        GameObject loadingBar = loadingScreen.GetComponent<Transform>().GetChild(0).GetChild(0).gameObject;
+
+        //Add some sort of transition
+
+
+        //Turn the loading screen on
+        background.SetActive(true);
+
+        //Just wait
+        yield return new WaitForSeconds(2);
+
+        //Start actually loading the new scene
         AsyncOperation opereration = SceneManager.LoadSceneAsync(MainGame);
 
+        //Track the new scene progress
         while (!opereration.isDone)
         {
-            Debug.Log(opereration.progress);
+            //Calculate current progress
+            float CurrentProgress = Mathf.Clamp(opereration.progress / 0.9f, 1, 2);
 
+            //Get the bar fill object
+            GameObject barFill = loadingScreen.GetComponent<Transform>().GetChild(0).GetChild(0).GetChild(0).gameObject;
+
+            //Change the bar size
+            barFill.GetComponent<RectTransform>().localScale = new Vector3(CurrentProgress, 1, 1);
+
+            //Restart the loop
             yield return null;
         }
 
