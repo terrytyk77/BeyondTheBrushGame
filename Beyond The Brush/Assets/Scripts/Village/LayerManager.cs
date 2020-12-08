44f7c37eae4bb6e17c2bad6e0ffcd16086a94949
DESKTOP-O32329F\Darkness <terrytyk77@gmail.com>
2020-12-08 22:38:47 +0000

    Village Independent Environment Object Layering Working! And Enemy Health Bar UI

---
+++ Beyond The Brush/Assets/Scripts/Village/LayerManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Rendering;
+using UnityEngine;
+
+public class LayerManager : MonoBehaviour
+{
+    public GameObject player;
+    public Transform environmentObjects;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        foreach (Transform envObject in environmentObjects)
+        {
+            if (envObject.GetComponent<BoxCollider2D>())
+            {
+                if (envObject.transform.position.y + envObject.GetComponent<BoxCollider2D>().size.y/2 < player.transform.position.y)
+                {
+                    envObject.GetComponent<SortingGroup>().sortingLayerName = "ObjectsHoveringPlayer";
+                }
+                else
+                {
+                    envObject.GetComponent<SortingGroup>().sortingLayerName = "ObjectsUnderPlayer";
+                }
+            }
+            else
+            {
+                if (envObject.transform.position.y + envObject.GetComponent<CircleCollider2D>().radius / 2 < player.transform.position.y)
+                {
+                    envObject.GetComponent<SortingGroup>().sortingLayerName = "ObjectsHoveringPlayer";
+                }
+                else
+                {
+                    envObject.GetComponent<SortingGroup>().sortingLayerName = "ObjectsUnderPlayer";
+                }
+            }
+
+        }
+    }
+}
