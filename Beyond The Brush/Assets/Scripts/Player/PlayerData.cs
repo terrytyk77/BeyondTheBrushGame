0c7cd302d3cd7b789776de95cd2884e0a46ed060
prooheckcp <vasco.soares.2001@gmail.com>
2020-12-27 21:42:08 +0000

    Ended the talent tree

--- Beyond The Brush/Assets/Scripts/Player/PlayerData.cs
+++ Beyond The Brush/Assets/Scripts/Player/PlayerData.cs
@@ -21,7 +21,7 @@ public class PlayerData : MonoBehaviour
         static private string _username = "Offline Player";
         static private int _level = 1;
         static private int _exp = 0;
-        static private int _resources = 50000;
+        static private int _resources = 0;
         static private int _gold = 0;
         static private string _email;
 
