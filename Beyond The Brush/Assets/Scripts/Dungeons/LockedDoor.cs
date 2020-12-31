53c36d22ff179a485daf6771d20939900b486ab8
DESKTOP-O32329F\Darkness <terrytyk77@gmail.com>
2020-12-31 02:52:57 +0000

    Locked Doors, And Unlockable With Drawing! Ai bug fix!

---
+++ Beyond The Brush/Assets/Scripts/Dungeons/LockedDoor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class LockedDoor : MonoBehaviour
+{
+    public Tile doorTile;
+
+    private GameObject collisionObject;
+    private Tilemap collisionTilemap;
+
+    private Vector3Int collisionTile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collisionObject = GameObject.FindGameObjectWithTag("CollisionLayer");
+        collisionTilemap = collisionObject.GetComponent<Tilemap>();
+        collisionTile = collisionTilemap.WorldToCell(transform.position);
+        collisionTilemap.SetTile(collisionTile, doorTile);
+    }
+
+    public void DestroyDoor()
+    {
+        if(gameObject != null)
+        {
+            collisionTilemap.SetTile(collisionTile, null);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Beyond The Brush/Assets/Scripts/Dungeons/LockedDoorTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoorTrigger : MonoBehaviour
+{
+    public GameObject doorToOpen;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Drawable")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Stone(Clone)":
+                    {
+                        if(doorToOpen != null)
+                        {
+                            doorToOpen.GetComponent<LockedDoor>().DestroyDoor();
+                        }
+                        break;
+                    }
+                case "Crate(Clone)":
+                    {
+                        break;
+                    }
+            }
+        }
+    }
+}
