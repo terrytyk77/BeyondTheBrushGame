dde8b57657ec66456e141102a114ef44d129386c
DESKTOP-O32329F\Darkness <terrytyk77@gmail.com>
2021-01-05 20:25:15 +0000

    Steel Chopper Implemented && Randomized Chests!

---
+++ Beyond The Brush/Assets/Scripts/Chest/Chest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Chest : MonoBehaviour
+{
+    //Coins         ||
+        public int MaxCoinAmount = 1;
+        private int CoinAmountRandomizer;
+    //--------------||
+
+    //Collision     ||
+        public Tile InvisibleCollisionTile;
+
+        private GameObject collisionObject;
+        private Tilemap collisionTilemap;
+
+        private Vector3Int collisionTile;
+
+        Vector3 HalfTile;
+    //--------------||
+
+    void Start()
+    {
+        collisionObject = GameObject.FindGameObjectWithTag("CollisionLayer");
+        collisionTilemap = collisionObject.GetComponent<Tilemap>();
+        HalfTile = new Vector3(collisionTilemap.cellSize.x / 2, collisionTilemap.cellSize.y / 2, 0);
+
+        //To Place The Chest Pixel Accurate With the Tilemap
+        transform.position = collisionTilemap.CellToWorld(collisionTilemap.WorldToCell(transform.position)) + HalfTile;
+
+        //Set Invisible Collision Tile Behind Chest
+        collisionTile = collisionTilemap.WorldToCell(transform.position);
+        collisionTilemap.SetTile(collisionTile, InvisibleCollisionTile);
+    }
+
+    private void OnDestroy()
+    {
+        if(collisionTilemap != null)
+        {
+            collisionTilemap.SetTile(collisionTile, null);
+        }
+    }
+}
