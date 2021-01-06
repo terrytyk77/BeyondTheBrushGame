16ac025828a1b338e72f515eedb9ddfbbae3e8a7
DESKTOP-O32329F\Darkness <terrytyk77@gmail.com>
2021-01-06 23:18:26 +0000

    Coin Fix

--- Beyond The Brush/Assets/Scripts/Chest/Coin.cs
+++ Beyond The Brush/Assets/Scripts/Chest/Coin.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public int goldValue;
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
