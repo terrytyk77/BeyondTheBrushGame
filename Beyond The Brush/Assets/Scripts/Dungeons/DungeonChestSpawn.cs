95f6c32362d170046e248b97632396642d228348
DESKTOP-O32329F\Darkness <terrytyk77@gmail.com>
2021-01-07 00:51:49 +0000

    Room Completed Working! Coin Added Shadows and animation!

--- Beyond The Brush/Assets/Scripts/Dungeons/DungeonChestSpawn.cs
+++ Beyond The Brush/Assets/Scripts/Dungeons/DungeonChestSpawn.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class DungeonChestSpawn : MonoBehaviour
 {
     private GameObject chestSpawnPoint;
+    private GameObject chestParent;
+
     private Transform[] childrenChestSpawnPoint;
     private GameObject spawnedChest;
     private int chestRandomPosition;
@@ -15,9 +17,12 @@ public class DungeonChestSpawn : MonoBehaviour
 
     private int ChestTypeRandomizer;
 
+    private GameObject EnemyParent;
+
     void Start()
     {
         chestSpawnPoint = GameObject.Find("ChestSpawnPoint");
+        chestParent = GameObject.Find("Chests");
 
         if (chestSpawnPoint != null)
         {
@@ -44,7 +49,20 @@ public class DungeonChestSpawn : MonoBehaviour
             }
             
             //Adjust Parent
-            spawnedChest.transform.SetParent(transform);
+            spawnedChest.transform.SetParent(chestParent.transform);
+        }
+    }
+
+    void Update()
+    {
+        //Checks If Room Is Completed!
+        EnemyParent = GameObject.Find("Enemies");
+        chestParent = GameObject.Find("Chests");
+
+        if (EnemyParent.transform.childCount == 0 && chestParent.transform.childCount == 0)
+        {
+            CurrentDungeonData currentData = GameObject.FindGameObjectWithTag("proceduralData").GetComponent<CurrentDungeonData>();
+            currentData.getRoomViaCords(currentData.currentRoom).setCompleted(true);
         }
     }
 }
