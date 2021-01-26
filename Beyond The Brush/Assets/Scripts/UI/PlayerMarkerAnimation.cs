bf5ee666fc2fabc2cbe8cfd95d2061288f6549b4
prooheckcp <vasco.soares.2001@gmail.com>
2021-01-26 09:52:32 +0000

    Added player marker animation and circular minimap

---
+++ Beyond The Brush/Assets/Scripts/UI/PlayerMarkerAnimation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMarkerAnimation : MonoBehaviour
+{
+    //Variables||
+
+        private Vector2 startingPoint;
+        private bool direction = true;
+
+        private float sizeLimit = 3.5f;
+        private float speed = 0.09f;
+    //_________||
+
+    private void Start()
+    {
+        startingPoint = gameObject.GetComponent<Transform>().localScale;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Transform markerSize = gameObject.GetComponent<Transform>();
+        if (direction)
+        {
+            if(markerSize.localScale.x + speed > startingPoint.x + sizeLimit)
+                markerSize.localScale = new Vector2(startingPoint.x + sizeLimit, startingPoint.x + sizeLimit);
+            else
+                markerSize.localScale = new Vector2(markerSize.localScale.x + speed, markerSize.localScale.y + speed);
+        }
+        else
+        {
+            if (markerSize.localScale.x - speed < startingPoint.x - sizeLimit)
+                markerSize.localScale = new Vector2(startingPoint.x - sizeLimit, startingPoint.x - sizeLimit);
+            else
+                markerSize.localScale = new Vector2(markerSize.localScale.x - speed, markerSize.localScale.y - speed);
+        }
+
+        if (markerSize.localScale.y <= startingPoint.x - sizeLimit)
+            direction = true;
+        else if (markerSize.localScale.y >= startingPoint.x + sizeLimit)
+            direction = false;
+
+    }
+}
